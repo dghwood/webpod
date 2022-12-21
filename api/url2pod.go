7841762a36274ea92c62531e3ad3c0a67bc048b4
Duncan Wood <dgh.wood@gmail.com>
2022-12-21 20:22:00 +0000

    adding timestamp to api

--- api/url2pod.go
+++ api/url2pod.go
@@ -6,21 +6,25 @@ import (
 	storage "github.com/dghwood/webpod/storage"
 	t2s "github.com/dghwood/webpod/t2s"
 	"log"
+	"time"
 )
 
 type URL2PodRequest struct {
-	URL string `json:"url"`
+	URL       string    `json:"url"`
+	Timestamp time.Time `json:"timestamp"`
 }
 
 type URL2PodResponse struct {
+	Timestamp  time.Time     `json:"timestamp"`
 	ArticleURL string        `json:"article_url"`
 	Article    parse.Article `json:"article"`
 	AudioURL   string        `json:"audio_url"`
 }
 
 func URL2Pod(request URL2PodRequest) (URL2PodResponse, bool) {
-	resp := URL2PodResponse{ArticleURL: request.URL}
 
+	resp := URL2PodResponse{ArticleURL: request.URL}
+	resp.Timestamp = request.Timestamp
 	article, _ := parse.ParseArticle(request.URL)
 	resp.Article = article
 	log.Println("HELLO", len(article.Text))
